d8e1b9a040a6cc21d0264eb4350fbefbaa2f583c
Nik <90584544+NikTern@users.noreply.github.com>
2023-04-11 22:46:43 +1000

    heroku update

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -53,10 +53,6 @@ module.exports = () => {
           use: ['style-loader', 'css-loader'],
         },
         {
-          test: /\.html$/,
-          loader: 'html-loader',
-        },
-        {
           test: /\.(png|svg|jpg|jpeg|gif)$/i,
           type: 'asset/resource',
         },

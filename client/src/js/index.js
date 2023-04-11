2ba46add0c629abf9fdacbed92c6e83c0f7526c4
Nik <90584544+NikTern@users.noreply.github.com>
2023-04-11 23:25:29 +1000

    logo fix attempt 3

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -2,9 +2,6 @@ import { Workbox } from 'workbox-window';
 import Editor from './editor';
 import './database';
 import '../css/style.css';
-import logo from '../images/logo.png';
-
-document.getElementById('logo-icon').src = logo;
 
 const main = document.querySelector('#main');
 main.innerHTML = '';
